0610e49100b3468bcad894dd8a6d0d8279954519
Keith Lashley <lashleykeith@gmail.com>
2021-10-30 19:37:01 +0900

    52 - Create Order - TypeError
    When we try to place an order we get this error TypeError: Cannot destructure property 'order' of 'orderCreate' as it is undefined.

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <main className="py-5">
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/login' component={LoginScreen} exact/>
+          <Route path='/register' component={RegisterScreen} exact/>
+          <Route path='/profile' component={ProfileScreen} exact/>
+          <Route path='/shipping' component={ShippingScreen} exact/>
+          <Route path='/placeorder' component={PlaceOrderScreen} exact/>
+          <Route path='/payment' component={PaymentScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} exact/>
+          <Route path='/cart/:id?' component={CartScreen} exact/>
+
+        </Container>
+      </main>
+
+      <Footer/>
+    </Router>
+
+  );
+}
+
+export default App;
